7ad2157ee71dfacc57cd3e430421c29a6dbe7486
Antonio B <bojorquez29an@gmail.com>
2024-01-08 02:45:13 -0700

    si

--- lib/app/repository/orden_repository.dart
+++ lib/app/repository/orden_repository.dart
@@ -1,18 +1,16 @@
-import 'dart:async';
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:proyecto_final/app/models/orden.dart';
 import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
+// import 'package:dio/dio.dart';
 
 class OrdenRepository {
-  final dio = Dio();
   Future<List<OrdenData>> getAllOrden() async {
     // final dio = Dio();
     // Response response = await dio
     //     .get('https://ms-servicio-hoja.onrender.com/hoja_servicio/all');
     var url = Uri.https('ms-servicio-hoja.onrender.com', '/hoja_servicio/all');
-
     var response = await http.get(url);
     if (response.statusCode == 200) {
       List<dynamic> jsonData = json.decode(response.body);
@@ -23,17 +21,6 @@ class OrdenRepository {
       List<OrdenData> ordenes = [];
       return ordenes;
     }
-  }
-
-  Future<List<OrdenData>> getAllOrdenes() async {
-    final response = await dio
-        .get('https://ms-servicio-hoja.onrender.com/hoja_servicio/all');
-
-    List<dynamic> ordenes = response.data;
-    ordenes.map((data) => print(data['empresa']));
-
-    List<OrdenData> prueba = [];
-    return prueba;
   }
 
   // Future<void> createHoja(OrdenData ordendata) async {
@@ -57,64 +44,14 @@ class OrdenRepository {
 
   void createHoja(OrdenData ordenData) async {
     final dio = Dio();
-    String url = "http://localhost:8080/hoja_servicio";
+    String url = "https://ms-servicio-hoja.onrender.com/hoja_servicio";
     var body = ordenData.toJson();
-
-    var prueba = {
-      "empresa": ordenData.empresa,
-      "zona": ordenData.zona,
-      "agencia": ordenData.agencia,
-      "horaInicio": ordenData.horaInicio,
-      "horaTermino": ordenData.horaTermino,
-      "noEquipo": ordenData.noEquipo,
-      "noEquipoSerie": ordenData.noEquipoSerie,
-      "noInventario": ordenData.noInventario,
-      "preventivoCompleto": ordenData.preventivoCompleto,
-      "correctivo": ordenData.correctivo,
-      "verificarComunicacionMonitoreo":
-          ordenData.verificarComunicacionMonitoreo,
-      "verificarComunicacionSicom": ordenData.verificarComunicacionSicom,
-      "preubasAceptacionBilletes": ordenData.preubasAceptacionBilletes,
-      "preubasAceptacionBilletesDesc": ordenData.pruebasDispensadoBilletesDesc,
-      "pruebasAceptadorMonedas": ordenData.pruebasAceptadorMonedas,
-      "pruebasDispensadoMonedas": ordenData.pruebasDispensadoMonedas,
-      "pruebasDispensadoMonedasDesc": " ",
-      "preubasDispensadoBilletes": ordenData.preubasDispensadoBilletes,
-      "pruebasDispensadoBilletesDesc": " ",
-      "pruebasImpresion": ordenData.pruebasImpresion,
-      "diagnosticoFallas": ordenData.diagnosticoFallas,
-      "mantenimientoGabinete": ordenData.mantenimientoGabinete,
-      "organizacionEstadoCableado": ordenData.organizacionEstadoCableado,
-      "mantenimientoPc": ordenData.mantenimientoPc,
-      "mantenimientoMonitor": ordenData.mantenimientoMonitor,
-      "mantenimientoEscaner": ordenData.mantenimientoEscaner,
-      "mantenimientoImpresora": ordenData.mantenimientoImpresora,
-      "mantenimientoTarjetaInterfaz": ordenData.mantenimientoTarjetaInterfaz,
-      "mantenimientoToneleros": ordenData.mantenimientoToneleros,
-      "mantenimientoDispensadorBilletes":
-          ordenData.mantenimientoDispensadorBilletes,
-      "mantenimientoAceptadorBilletes":
-          ordenData.mantenimientoAceptadorBilletes,
-      "mantenimientoAceptadorMonedas": false,
-      "ups": ordenData.ups.toJson(),
-      "refaciones": [],
-      "observaciones": ordenData.observaciones,
-      "estadoVerificacionUltimaVersionLiberada":
-          ordenData.estadoVerificacionUltimaVersionLiberada,
-      "verificacionUltimaVersionLiberada":
-          ordenData.verificacionUltimaVersionLiberada,
-      "actualizacionAntivirusCorporativo":
-          ordenData.actualizacionAntivirusCorporativo,
-      "verificaFechaHora": ordenData.verificaFechaHora,
-      "aceptaMantenimiento": ordenData.aceptaMantenimiento,
-      "detalleServicio": ordenData.detalleServicio,
-      "calidadServicio": ordenData.calidadServicio
-    };
-    print(prueba);
+    print(body);
     Response res = await dio
         .post(
       url,
-      data: prueba,
+      data: body,
+      options: Options(headers: {"Content-Type": "application/json"}),
     )
         .catchError((err) {
       print('Error ${err}');
